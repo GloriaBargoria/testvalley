4275aafe19a45634e33d3c693a7bfc16d2452bee
GloriaBargoria <bargoriagloria@gmail.com>
2024-02-22 05:04:47 +0300

    project setup and layout setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,13 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        
+      },
+      colors: {
+        valleyGreen: "#00d094",
+        valleyDark: "#333333",
+        valleyGray: "#999999",
+        valleyLight: "#dddddd",
       },
     },
   },
